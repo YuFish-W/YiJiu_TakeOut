9c3fe6fe996f283b8780f210f1b19aa9673b1160
YuFish-W <1219471845@qq.com>
2022-09-11 17:29:27 +0800

    增加购物车商品-1功能

--- src/main/java/com/yufish/yijiu/controller/ShoppingCartController.java
+++ src/main/java/com/yufish/yijiu/controller/ShoppingCartController.java
@@ -23,14 +23,16 @@ public class ShoppingCartController {
     @Autowired
     private ShoppingCartService shoppingCartService;
 
+
     /**
      * 添加购物车
+     *
      * @param shoppingCart
      * @return
      */
     @PostMapping("/add")
-    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart){
-        log.info("购物车数据:{}",shoppingCart);
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart) {
+        log.info("购物车数据:{}", shoppingCart);
 
         //设置用户id，指定当前是哪个用户的购物车数据
         Long currentId = BaseContext.getCurrentId();
@@ -39,27 +41,27 @@ public class ShoppingCartController {
         Long dishId = shoppingCart.getDishId();
 
         LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(ShoppingCart::getUserId,currentId);
+        queryWrapper.eq(ShoppingCart::getUserId, currentId);
 
-        if(dishId != null){
+        if (dishId != null) {
             //添加到购物车的是菜品
-            queryWrapper.eq(ShoppingCart::getDishId,dishId);
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
 
-        }else{
+        } else {
             //添加到购物车的是套餐
-            queryWrapper.eq(ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
+            queryWrapper.eq(ShoppingCart::getSetmealId, shoppingCart.getSetmealId());
         }
 
         //查询当前菜品或者套餐是否在购物车中
         //SQL:select * from shopping_cart where user_id = ? and dish_id/setmeal_id = ?
         ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
 
-        if(cartServiceOne != null){
+        if (cartServiceOne != null) {
             //如果已经存在，就在原来数量基础上加一
             Integer number = cartServiceOne.getNumber();
             cartServiceOne.setNumber(number + 1);
             shoppingCartService.updateById(cartServiceOne);
-        }else{
+        } else {
             //如果不存在，则添加到购物车，数量默认就是一
             shoppingCart.setNumber(1);
             shoppingCart.setCreateTime(LocalDateTime.now());
@@ -72,14 +74,15 @@ public class ShoppingCartController {
 
     /**
      * 查看购物车
+     *
      * @return
      */
     @GetMapping("/list")
-    public R<List<ShoppingCart>> list(){
+    public R<List<ShoppingCart>> list() {
         log.info("查看购物车...");
 
         LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(ShoppingCart::getUserId,BaseContext.getCurrentId());
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
         queryWrapper.orderByAsc(ShoppingCart::getCreateTime);
 
         List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
@@ -89,17 +92,49 @@ public class ShoppingCartController {
 
     /**
      * 清空购物车
+     *
      * @return
      */
     @DeleteMapping("/clean")
-    public R<String> clean(){
+    public R<String> clean() {
         //SQL:delete from shopping_cart where user_id = ?
 
         LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(ShoppingCart::getUserId,BaseContext.getCurrentId());
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
 
         shoppingCartService.remove(queryWrapper);
 
         return R.success("清空购物车成功");
+    }
+
+    /**
+     * 购物车商品-1，每发一次请求就该购物车商品的数量就减一，为0的时候删除
+     * @param shoppingCart 传入的可能是套餐id，也可能是菜品id，直接用shoppingCart接收
+     * @return
+     */
+    @PostMapping("/sub")
+    public R<ShoppingCart> sub(@RequestBody ShoppingCart shoppingCart) {
+        //得到当前用户id
+        Long currentId = BaseContext.getCurrentId();
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+
+        Long dishId = shoppingCart.getDishId();
+        if (dishId == null) {
+            Long setmealId = shoppingCart.getSetmealId();
+            queryWrapper.eq(ShoppingCart::getUserId,currentId).eq(ShoppingCart::getSetmealId,setmealId);
+        }else {
+            queryWrapper.eq(ShoppingCart::getUserId,currentId).eq(ShoppingCart::getDishId,dishId);
+        }
+        //得到当前用户该套餐id的购物车对象
+        shoppingCart = shoppingCartService.getOne(queryWrapper);
+
+        shoppingCart.setNumber(shoppingCart.getNumber() - 1);
+        //如果数量减一之后为0就删除该对象
+        if (shoppingCart.getNumber() == 0) {
+            shoppingCartService.removeById(shoppingCart.getId());
+        } else {
+            shoppingCartService.updateById(shoppingCart);
+        }
+        return R.success(shoppingCart);
     }
 }
