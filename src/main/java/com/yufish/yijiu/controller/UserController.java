5846a57149bf5691743bacfe5718f32584a5b00c
YuFish-W <1219471845@qq.com>
2022-09-12 09:52:31 +0800

    用户保存功能增加邮箱保存

--- src/main/java/com/yufish/yijiu/controller/UserController.java
+++ src/main/java/com/yufish/yijiu/controller/UserController.java
@@ -99,23 +99,25 @@ public class UserController {
 
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
             queryWrapper.eq(User::getPhone,phone);
+            queryWrapper.eq(User::getEmail,email);
 
             User user = userService.getOne(queryWrapper);
             if(user == null){
                 //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册
                 user = new User();
                 user.setPhone(phone);
+                user.setEmail(email);
                 user.setStatus(1);
                 userService.save(user);
             }
             session.setAttribute("user",user.getId());
 
             //如果用户登录成功，删除Redis中缓存的验证码
-            redisTemplate.delete(phone);
+            redisTemplate.delete(email);
 
             return R.success(user);
         }
-        return R.error("登录失败");
+        return R.error("验证码错误");
     }
 
     /**
