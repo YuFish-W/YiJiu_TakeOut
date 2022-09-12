bd98bf5ab8af75164b0ca9d99a1741c8acbf6c42
YuFish-W <1219471845@qq.com>
2022-09-12 08:54:11 +0800

    增加邮箱接收验证码功能

--- src/main/java/com/yufish/yijiu/controller/UserController.java
+++ src/main/java/com/yufish/yijiu/controller/UserController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.yufish.yijiu.common.R;
 import com.yufish.yijiu.entity.User;
 import com.yufish.yijiu.service.UserService;
+import com.yufish.yijiu.utils.EmailUtils;
 import com.yufish.yijiu.utils.ValidateCodeUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,10 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpSession;
@@ -34,21 +32,24 @@ public class UserController {
     @Autowired
     private RedisTemplate redisTemplate;
 
+
     /**
-     * 发送手机短信验证码
-     * @param user
+     * 接收传过来的{email=''}对象
+     * @param map
      * @return
      */
     @PostMapping("/sendMsg")
     @ApiOperation(value = "发送验证码接口")
-    public R<String> sendMsg(@RequestBody User user){
+    public R<String> sendMsg(/*@RequestBody User user,*/@RequestBody Map map){
         //获取手机号
-        String phone = user.getPhone();
+//        String phone = user.getPhone();
 
-        if(StringUtils.isNotEmpty(phone)){
+        String  email = (String)map.get("email");
+        if(StringUtils.isNotEmpty(email)){
             //生成随机的4位验证码
             String code = ValidateCodeUtils.generateValidateCode(4).toString();
             log.info("code={}",code);
+            EmailUtils.sendMessages(email,code);
 
             //调用阿里云提供的短信服务API完成发送短信
             //SMSUtils.sendMessage("一酒外卖","",phone,code);
@@ -57,7 +58,8 @@ public class UserController {
             //session.setAttribute(phone,code);
 
             //将生成的验证码缓存到Redis中，并且设置有效期为5分钟
-            redisTemplate.opsForValue().set(phone,code,5,TimeUnit.MINUTES);
+//            redisTemplate.opsForValue().set(phone,code,5,TimeUnit.MINUTES);
+            redisTemplate.opsForValue().set(email,code,5,TimeUnit.MINUTES);
 
             return R.success("手机验证码短信发送成功");
         }
@@ -82,11 +84,14 @@ public class UserController {
         //获取验证码
         String code = map.get("code").toString();
 
+        //获取邮箱
+        String email = map.get("email").toString();
+
         //从Session中获取保存的验证码
         //Object codeInSession = session.getAttribute(phone);
 
         //从Redis中获取缓存的验证码
-        Object codeInSession = redisTemplate.opsForValue().get(phone);
+        Object codeInSession = redisTemplate.opsForValue().get(email);
 
         //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
         if(codeInSession != null && codeInSession.equals(code)){
---
+++ src/main/java/com/yufish/yijiu/utils/EmailUtils.java
@@ -0,0 +1,73 @@
+package com.yufish.yijiu.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import java.util.Date;
+import java.util.Properties;
+
+@Slf4j
+public class EmailUtils {
+
+    public static void sendMessages(String email, String code) {
+        try {
+            //创建Properties 类用于记录邮箱的一些属性
+            final Properties props = new Properties();
+            //表示SMTP发送邮件，必须进行身份验证
+            props.put("mail.smtp.auth", "true");
+            //此处填写SMTP服务器
+            props.put("mail.smtp.host", "smtp.qq.com");
+            //端口号，QQ邮箱给出了两个端口，这里给出587
+            props.put("mail.smtp.port", "587");
+            //此处填写你的账号
+            props.put("mail.user", "1219471845@qq.com");
+            //此处的密码就是前面说的16位STMP口令
+            //获取口令
+            props.put("mail.password", "robksqrnzgilfghh");
+            //构建授权信息，用于进行SMTP进行身份验证
+            Authenticator authenticator = new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    // 用户名、密码
+                    String userName = props.getProperty("mail.user");
+                    String password = props.getProperty("mail.password");
+                    return new PasswordAuthentication(userName, password);
+                }
+            };
+            //使用环境属性和授权信息，创建邮件会话
+            Session mailSession = Session.getInstance(props, authenticator);
+            //创建邮件消息
+            MimeMessage message = new MimeMessage(mailSession);
+            //设置发件人
+            InternetAddress form = new InternetAddress(props.getProperty("mail.user"));
+            message.setFrom(form);
+
+            //设置收件人的邮箱
+            InternetAddress to = new InternetAddress(email);
+            message.setRecipient(Message.RecipientType.TO, to);
+
+            //设置邮件主题
+            message.setSubject("测试邮件");
+            //设置消息日期
+            message.setSentDate(new Date());
+
+            //html文件
+            StringBuilder sb = new StringBuilder();
+            sb.append("<h1>" + code + "</h1>");
+            //设置邮件的内容体
+            message.setContent(sb.toString(), "text/html;charset=UTF-8");
+
+            //最后当然就是发送邮件
+            Transport.send(message);
+            log.info("发送成功！");
+        } catch (AddressException e) {
+            log.info("发送失败！,{}",e.getMessage());
+            e.printStackTrace();
+        } catch (javax.mail.MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
