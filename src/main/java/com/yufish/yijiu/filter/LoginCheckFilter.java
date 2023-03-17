583784c862cf9e70293b302adcb981698166aaf4
YuFish-W <1219471845@qq.com>
2023-03-17 16:05:08 +0800

    修改过滤器

--- src/main/java/com/yufish/yijiu/filter/LoginCheckFilter.java
+++ src/main/java/com/yufish/yijiu/filter/LoginCheckFilter.java
@@ -16,9 +16,9 @@ import java.util.regex.Pattern;
 /**
  * 检查用户是否已经完成登录
  */
-@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
+@WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
 @Slf4j
-public class LoginCheckFilter implements Filter{
+public class LoginCheckFilter implements Filter {
     //路径匹配器，支持通配符
     public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
 
@@ -35,7 +35,7 @@ public class LoginCheckFilter implements Filter{
         String referer = request.getHeader("Referer");
 
 
-         log.info("拦截到请求：{}",requestURI);
+        log.info("拦截到请求：{}", requestURI);
 
         //定义不需要处理的请求路径
         String[] urls = new String[]{
@@ -56,33 +56,33 @@ public class LoginCheckFilter implements Filter{
         boolean check = check(urls, requestURI);
 
         //3、如果不需要处理，则直接放行
-        if(check){
-            log.info("本次请求{}不需要处理",requestURI);
-            filterChain.doFilter(request,response);
+        if (check) {
+            log.info("本次请求{}不需要处理", requestURI);
+            filterChain.doFilter(request, response);
             return;
         }
 
         //4-1、判断登录状态，如果已登录，则直接放行,并且判断是否是前端页面的请求，不能因为后端登陆了把前端也放行
         // && Pattern.matches("http(s)?://.*/backend.*",referer)
-        if(request.getSession().getAttribute("employee") != null){
-            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("employee"));
+        if (request.getSession().getAttribute("employee") != null && Pattern.matches("http(s)?://.*/backend.*", referer)) {
+            log.info("用户已登录，用户id为：{}", request.getSession().getAttribute("employee"));
 
             Long empId = (Long) request.getSession().getAttribute("employee");
             BaseContext.setCurrentId(empId);
 
-            filterChain.doFilter(request,response);
+            filterChain.doFilter(request, response);
             return;
         }
 
         //4-2、判断登录状态，如果已登录，则直接放行
         // && Pattern.matches("http(s)?://.*/front.*",referer)
-        if(request.getSession().getAttribute("user") != null){
-            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("user"));
+        if (request.getSession().getAttribute("user") != null && Pattern.matches("http(s)?://.*/front.*", referer)) {
+            log.info("用户已登录，用户id为：{}", request.getSession().getAttribute("user"));
 
             Long userId = (Long) request.getSession().getAttribute("user");
             BaseContext.setCurrentId(userId);
 
-            filterChain.doFilter(request,response);
+            filterChain.doFilter(request, response);
             return;
         }
 
@@ -95,14 +95,15 @@ public class LoginCheckFilter implements Filter{
 
     /**
      * 路径匹配，检查本次请求是否需要放行
+     *
      * @param urls
      * @param requestURI
      * @return
      */
-    public boolean check(String[] urls,String requestURI){
+    public boolean check(String[] urls, String requestURI) {
         for (String url : urls) {
             boolean match = PATH_MATCHER.match(url, requestURI);
-            if(match){
+            if (match) {
                 return true;
             }
         }
